6fb0042365bb327f07568866012e3b42a3d87f63
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2015-07-22 10:15:34 +0200

    AtomFeed.java - some cleanup
    Removing unused variables mainly

--- inspire-atom/src/main/java/org/fao/geonet/services/inspireatom/AtomFeed.java
+++ inspire-atom/src/main/java/org/fao/geonet/services/inspireatom/AtomFeed.java
@@ -1,7 +1,5 @@
 package org.fao.geonet.services.inspireatom;
 
-import java.util.Map;
-
 import javassist.NotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,14 +9,11 @@ import jeeves.server.dispatchers.ServiceManager;
 
 import org.apache.commons.lang.StringUtils;
 import org.fao.geonet.ApplicationContextHolder;
-import org.fao.geonet.Logger;
 import org.fao.geonet.constants.Geonet;
-import org.fao.geonet.domain.Metadata;
 import org.fao.geonet.exceptions.BadParameterEx;
 import org.fao.geonet.exceptions.MetadataNotFoundEx;
 import org.fao.geonet.inspireatom.util.InspireAtomUtil;
 import org.fao.geonet.kernel.DataManager;
-import org.fao.geonet.kernel.search.SearchManager;
 import org.fao.geonet.kernel.setting.SettingManager;
 import org.fao.geonet.utils.Log;
 import org.jdom.Element;
@@ -31,16 +26,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.NativeWebRequest;
 
-import com.google.common.collect.Maps;
-
 /**
  * Created by fgravin on 7/15/15.
  */
 
 @Controller
 public class AtomFeed {
-
-    private Logger logger = Log.createLogger(Geonet.ATOM);
 
     @RequestMapping(value = "/{uiLang}/atom.service/{uuid}")
     @ResponseBody
@@ -61,6 +52,13 @@ public class AtomFeed {
         return getAtomFeed(uiLang, null, webRequest);
     }
 
+    @RequestMapping("/{uiLang}/atom.local.describe")
+    @ResponseBody
+    public HttpEntity<byte[]> localDescribe(@PathVariable String uiLang, NativeWebRequest webRequest) {
+
+        return null;
+    }
+
     /**
      *
      * @param uiLang main geonetwork language
@@ -73,7 +71,6 @@ public class AtomFeed {
         ServiceContext context = createServiceContext(uiLang,
                 webRequest.getNativeRequest(HttpServletRequest.class));
 
-        SearchManager searchManager = context.getBean(SearchManager.class);
         SettingManager sm = context.getBean(SettingManager.class);
         DataManager dm = context.getBean(DataManager.class);
 
@@ -83,10 +80,6 @@ public class AtomFeed {
             Log.info(Geonet.ATOM, "Inspire is disabled");
             throw new BadParameterEx("system/inspire/enable. Please activate INSPIRE before trying to use the service.", inspireEnable);
         }
-
-        Map<String, Object> results = Maps.newLinkedHashMap();
-
-        Metadata iso19139Metadata;
 
         // Check if metadata exists
         String id = dm.getMetadataId(uuid);
@@ -103,6 +96,7 @@ public class AtomFeed {
         baseUrl = baseUrl.substring(0, baseUrl.length()-5);
         String lang = context.getLanguage();
         Element inputDoc = InspireAtomUtil.createInputElement(schema, md, dm, baseUrl, lang);
+
         String atomFeed = InspireAtomUtil.convertIso19119ToAtomFeed(schema, inputDoc, dm, true);
         
         return writeOutResponse(atomFeed);
@@ -117,6 +111,7 @@ public class AtomFeed {
         byte[] documentBody = content.getBytes();
 
         HttpHeaders header = new HttpHeaders();
+        // TODO: character-set encoding ?
         header.setContentType(new MediaType("application", "xml"));
         header.setContentLength(documentBody.length);
         return new HttpEntity<byte[]>(documentBody, header);
